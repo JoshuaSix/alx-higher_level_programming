d60369e5d4ef2305cf3769cf7693d295f5ce180a
JoshuaSix <nsiah.joshua.6@gmail.com>
2023-01-07 13:42:03 -0800

    alx-higher_level_programming

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
+
 #!/usr/bin/python3
 
-f __name__ == "__main__":
+if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div 
     import sys
 
     if len(sys.argv) - 1 != 3:
