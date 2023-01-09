2cd8fea0535ad924c93e3e8a5dc1678c0c67bf92
JoshuaSix <nsiah.joshua.6@gmail.com>
2023-01-08 16:01:56 -0800

    alx-higher_level_programming

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,5 @@
-2-replace_in_list.py
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
+    if not idx < 0 and not idx >= len(my_list):
         del my_list[idx]
     return my_list
